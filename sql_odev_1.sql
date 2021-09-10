07b1fae69eff1c62eb3a17427989909ff31bd123
Furkan Güver <76595653+guverfurkan@users.noreply.github.com>
2021-09-10 17:20:12 +0200

    Update sql_odev_1.sql

--- sql_odev_1.sql
+++ sql_odev_1.sql
@@ -4,11 +4,11 @@ ORDER BY title
 
 --film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız
 SELECT * FROM film
-WHERE length > 60 and length < 75
+WHERE length > 60 AND length < 75
 
 --film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız
 SELECT * FROM film
-WHERE rental_rate = 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99
+WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99
 
 --customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 SELECT last_name FROM customer
@@ -16,4 +16,4 @@ WHERE first_name = 'Mary'
 
 --film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 SELECT * FROM film
-where length <= 50 and not (rental_rate = 2.99 or rental_rate = 4.99)
+WHERE length <= 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99)
